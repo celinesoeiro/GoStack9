218b6d839ab96ed9f0c25c40252372b20f68a11f
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-11-14 17:43:16 -0300

    Validação do agendamento

--- goBarber/src/app/controllers/AppointmentController.js
+++ goBarber/src/app/controllers/AppointmentController.js
@@ -1,4 +1,6 @@
+/* eslint-disable class-methods-use-this */
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -22,13 +24,34 @@ class AppointmentController {
     if (!isProvider) {
       return res
         .status(401)
-        .json({ error: 'Appointment can only be created for providers.' });
+        .json({ error: 'Appointment can only be assingned to providers.' });
     }
+    // verificando se a data é válida, ou seja, não aconteceu ainda
+    const hourStart = startOfHour(parseISO(date));
 
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permited.' });
+    }
+    // Verificando se o agendamento é possível
+    const notAvailable = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (notAvailable) {
+      return res.status(400).json({
+        error:
+          'Appointment not available. There is already an appointment at the same time.',
+      });
+    }
+    // Cria o agendamento
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json({ appointment });
