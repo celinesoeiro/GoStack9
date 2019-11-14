518de5154d91f1303ec83e88336d9204e34d5167
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-11-14 16:05:18 -0300

    Agendamento de Serviços

--- goBarber/src/routes.js
+++ goBarber/src/routes.js
@@ -10,6 +10,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 // Middleware de autenticação
 import authMiddleware from './app/middlewares/auth';
 // Criação das rotas
@@ -27,4 +28,6 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 // Listando os usuários providers
 routes.get('/providers', ProviderController.index);
+// Marcando um horário
+routes.post('/appointments', AppointmentController.store);
 export default routes;
