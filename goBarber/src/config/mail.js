4adf3f8b616bf1333354af5a6d6c78dfed822cef
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-11-15 16:39:47 -0300

    Configurando nodemailer

---
+++ goBarber/src/config/mail.js
@@ -0,0 +1,15 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'c79c15c01a05a3',
+    pass: '301b3127df386d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// vai usar o mailtrap porém só funciona para ambientes de desenvolvimento,
+// uma vez online, não funciona mais.
