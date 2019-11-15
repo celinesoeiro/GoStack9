16d21b44af3ee65a9e9e29ff27bb524904b6f0dc
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-11-15 14:53:36 -0300

    Configurando o MongoDB
    Utilizei o mongodb atlas. Peguei o link de conexão com a aplicação para node.

--- goBarber/src/database/index.js
+++ goBarber/src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -11,6 +12,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -19,6 +21,19 @@ class Database {
     models.map(model => model.init(this.connection));
     models.map(
       model => model.associate && model.associate(this.connection.models)
+    );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://omnistack:omnistack@cluster0-shard-00-00-bubnn.mongodb.net:27017,cluster0-shard-00-01-bubnn.mongodb.net:27017,cluster0-shard-00-02-bubnn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+      // 'mongodb+srv://celine:asenhaE25@gobarber-asajd.mongodb.net/test?retryWrites=true&w=majority',
+      // 'mongodb://celine:asenhaE25@gobarber-shard-00-00-asajd.mongodb.net:27017,gobarber-shard-00-01-asajd.mongodb.net:27017,gobarber-shard-00-02-asajd.mongodb.net:27017/test?ssl=true&replicaSet=goBarber-shard-0&authSource=admin&retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
     );
   }
 }
