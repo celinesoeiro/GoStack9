fdb23472029a0dc3f93fa4f3185134339e347ef3
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-12-06 16:27:35 -0300

    F3-M4 - Ciclo de vida do componente

--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -4,12 +4,28 @@ import TechItem from './TechItem';
 class TechList extends Component{
   state = {
     newTech: '',
-    techs: [
-      "Node.js",
-      "ReactJS",
-      "React Native",
-    ]
+    techs: []
   };
+
+  // Executado assim que o componente aparece na tela.
+  componentDidMount(){
+    const techs = localStorage.getItem('techs');
+    if (techs){
+      this.setState({techs: JSON.parse(techs)});
+    }
+  }
+
+  // Executado quando há alterações nas props ou estado
+  // Recebe prevProps e prevState, quando não usa um deles coloca _
+  componentDidUpdate(_,prevState){
+    if(prevState !== this.state.techs){
+      localStorage.setItem('techs',JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  // Server pra limpar qualquer resquicio do componente que deixou de existir
+  // componentWillUnmount(){}
 
   handleInputChange = e => {
     this.setState({newTech: e.target.value});
