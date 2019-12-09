39eef1cddc894d391c51e3d52df87c959c6f306e
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-12-09 12:05:32 -0300

    Desafio 04

--- desafio04/src/components/Post.js
+++ desafio04/src/components/Post.js
@@ -1,28 +1,30 @@
 import React from 'react';
 
-function postHeader({author,date}){
+function PostHeader({author,date}){
   return(
     <div className="post-header">
       <img className="post-avatar" src={author.avatar}/>
-      <div className = "post-header-details">
+      <div className = "details">
         <span>{author.name}</span>
-        <span>{date}</span>
+        <span className="date">{date}</span>
       </div>
     </div>
   );
 }
 
-function postBody({comments}){
+function PostBody({comments}){
   return(
     <div className = "post-body">
-      {comments.map(comment => 
-        <div key={comment.id}>
-          <img className="post-avatar" src={comment.author.avatar}/>
-          <p className="comment-p">
-            <span>{comment.author.name}</span>
-            {comment.content}
-          </p>
-        </div>)}  
+      <div className = "divider" />
+        {comments.map(comment => (
+          <div key={comment.id} className="comment">
+            <img className="post-avatar" src={comment.author.avatar}/>
+            <p>
+              <span>{comment.author.name}</span>
+              {comment.content}
+            </p>
+          </div>
+        ))}
     </div>
   );
 }
@@ -30,9 +32,9 @@ function postBody({comments}){
 function Post({author,date,content,comments}){
   return(
     <div className="post">
-      <postHeader author={author} date={date}/>
-      <p className="content-p">{content}</p>
-      <postBody comments={comments}/>
+      <PostHeader author={author} date={date}/>
+      <p className="post-content">{content}</p>
+      <PostBody comments={comments}/>
     </div>
   );
 }
--- desafio04/src/components/PostList.js
+++ desafio04/src/components/PostList.js
@@ -20,7 +20,7 @@ class PostList extends Component {
               avatar: 'https://avatars2.githubusercontent.com/u/2254731?v=4'
             },
             date: '04 Jun 2019',
-            content: 'Comentário de Diego'
+            content: 'A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)'
           }
         ]
       },
@@ -31,8 +31,27 @@ class PostList extends Component {
           avatar: 'https://avatars0.githubusercontent.com/u/6298034?s=460&v=4'
         },
         date: '05 Jun 2019',
-        content: 'Fala de Lucas',
-        comments: []
+        content: 'Fala galera, beleza?\nEstou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia',
+        comments: [
+          {
+            id: 4,
+            author: {
+             name: 'Diego Fernandes',
+             avatar: 'https://avatars2.githubusercontent.com/u/2254731?v=4'
+            },
+            date: '06 Jun 2019',
+            content: 'Que bom, Lucas!'
+          },
+          {
+            id: 5,
+            author: {
+              name: 'Celine Soeiro',
+              avatar: 'https://avatars1.githubusercontent.com/u/52112166?s=460&v=4'
+            },
+            date: '07 Jun 2019',
+            content: 'Realmente é muito bom! Também estou fazendo.'
+          }
+        ]
       }
     ]
   };
