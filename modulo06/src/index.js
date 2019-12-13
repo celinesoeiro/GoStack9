2d829eac8d6f14bff78b032c12efdfd1bfd5050e
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-12-13 17:09:08 -0300

    Mod06 - Configurando StatusBar

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
+
 import './config/ReactotronConfig';
 
 import Routes from './routes';
@@ -6,6 +8,7 @@ import Routes from './routes';
 export default function App() {
   return (
     <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
       <Routes />
     </>
   );
