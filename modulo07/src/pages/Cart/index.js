c335fc89f876cbabddd77fa0e8bdaace5d137f80
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-12-14 19:03:33 -0300

    Estilização do carrinho

--- modulo07/src/pages/Cart/index.js
+++ modulo07/src/pages/Cart/index.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Carrinho</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th>Produto</th>
+            <th>Quantidade</th>
+            <th>Subtotal</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://i.pinimg.com/564x/d1/17/47/d117478237858686de3776c229c22833.jpg"
+                alt="Bota estrelada"
+              />
+            </td>
+            <td>
+              <strong>Bota de couro com detalhes bordados</strong>
+              <span>200,00</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>200</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>200</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
