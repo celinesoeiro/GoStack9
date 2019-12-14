36cf1357026b562936e710d5f4d624b200d75363
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-12-14 18:06:21 -0300

    Estilização da Home

--- modulo07/src/pages/Home/index.js
+++ modulo07/src/pages/Home/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ShoppingList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ShoppingList>
+      <li>
+        <img
+          src="https://i.pinimg.com/564x/d1/17/47/d117478237858686de3776c229c22833.jpg"
+          alt="Bota estrelada"
+        />
+        <strong>Bota de couro com detalhes bordados</strong>
+        <span>200,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ShoppingList>
+  );
 }
