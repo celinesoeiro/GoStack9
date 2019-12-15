52d0112031a3ff50063d65b32e2a4bf1861b0164
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-12-15 08:38:53 -0300

    Exibindo Quantidade

--- modulo07/src/pages/Home/index.js
+++ modulo07/src/pages/Home/index.js
@@ -30,6 +30,7 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
 
     return (
       <ShoppingList>
@@ -43,7 +44,8 @@ class Home extends Component {
               onClick={() => this.handleAddProduct(product)}
             >
               <div>
-                <MdAddShoppingCart size={16} color="#fff" />
+                <MdAddShoppingCart size={16} color="#fff" />{' '}
+                {amount[product.id] || 0}
               </div>
               <span>Adicionar ao carrinho</span>
             </button>
@@ -54,9 +56,16 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
 // converte actions do redux em propriedades dos componentes
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
 // O primeiro parâmetro é o mapStateToProps, como não existe, coloca null
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
