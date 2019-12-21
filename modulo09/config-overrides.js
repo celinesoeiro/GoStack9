8f820ce20276eb01c77325735c6ef3dfb0b6991f
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-12-21 17:40:50 -0300

    Utilizando root import

---
+++ modulo09/config-overrides.js
@@ -0,0 +1,13 @@
+// Arquivo node
+const { addBabelPlugin, override } = require('customize-cra');
+
+module.exports = override(
+  addBabelPlugin([
+    'babel-plugin-root-import',
+    {
+      rootPathSuffix: 'src',
+    },
+  ])
+);
+
+// permite acessar a pasta src utilizando o ~/ ao invés de procurar com os ../
