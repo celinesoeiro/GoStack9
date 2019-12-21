7445a3ff18a6b04db2f79dd6fa0680bb6e83bfd3
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-12-21 20:02:18 -0300

    Utilizando Unform
    @rocketseat/unform

--- modulo09/src/pages/SingUp/index.js
+++ modulo09/src/pages/SingUp/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
-
 export default function SingUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input placeholder="Nome completo" />
-        <input type="email" placeholder="Digite seu e-mail" />
-        <input type="password" placeholder="Digite sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="nome" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Digite seu e-mail" />
+        <Input name="password" type="password" placeholder="Digite sua senha" />
         <button type="submit">Criar conta</button>
         <Link to="/">Já sou cadastrado</Link>
-      </form>
+      </Form>
     </>
   );
 }
