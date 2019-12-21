d13581aeab7669462df389148f222b85edd1416b
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-12-21 20:14:24 -0300

    validações

--- modulo09/src/pages/SignIn/index.js
+++ modulo09/src/pages/SignIn/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const Schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um email válido.')
+    .required('Esse campo é obrigatório'),
+  password: Yup.string().required('Esse campo é obrigatório'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -12,7 +20,7 @@ export default function SignIn() {
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={Schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Digite seu e-mail" />
         <Input name="password" type="password" placeholder="Digite sua senha" />
         <button type="submit">Acessar</button>
