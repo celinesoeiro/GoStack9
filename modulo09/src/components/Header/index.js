a5f86afaea414ff166e479caa42395693dbfcf7f
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2019-12-22 09:56:00 -0300

    Estilizando notificações

--- modulo09/src/components/Header/index.js
+++ modulo09/src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Notifications from '../Notifications';
 import { Container, Content, Profile } from './styles';
 import logo from '~/assets/logoRoxo.svg';
 
@@ -14,6 +15,7 @@ export default function Header() {
         </nav>
 
         <aside>
+          <Notifications />
           <Profile>
             <div>
               <strong>Celine</strong>
